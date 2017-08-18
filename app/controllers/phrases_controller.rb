7dbf597adcb9e15cb3a8369ba93962ea6936ba79
Alex C <alex@swidjit.com>
2017-08-18 12:30:05 -0400

    simplification of chatting input

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,7 +1,7 @@
 class PhrasesController < ApplicationController
 
   def autocomplete
-    @phrases = Phrase.where('str LIKE ?',"%#{params[:q]}%")
+    @phrases = Phrase.where('str LIKE ?',"#{params[:q]}%")
     respond_to do |format|
       format.json { render :json => @phrases.collect{|phrase| {:id => phrase.id, :str => phrase.str}} }
     end
